293fef4b10c01d43ba4544fb97883e072c9c251f
Aytekin97 <aytecha.97@gmail.com>
2024-12-19 09:27:34 -0500

    Fix PreProcessingResponseSchema

--- schemas.py
+++ schemas.py
@@ -1,9 +1,15 @@
 from pydantic import BaseModel
+from typing import List
 
 
 class ChunkSummaryResponseSchema(BaseModel):
     summary: str
 
+class TableSchema(BaseModel):
+    table_name: str
+    columns: List[str]
+    rows: List[List[str]]
+
 class PreProcessingResponseSchema(BaseModel):
     text: str
-    tables: list[dict[str, list[list[str]]]]
+    tables: List[TableSchema]
